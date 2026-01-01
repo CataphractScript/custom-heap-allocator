6fc1bfc80ee8ccd98016d4b062df1a6489088ba0
Matin Amirpanah far <151240953+Matin0789@users.noreply.github.com>
2026-01-01 19:41:36 +0330

    refactor(heap.h): correct misleading text in heap.h

--- include/heap.h
+++ include/heap.h
@@ -9,11 +9,3 @@ void *halloc(size_t size);
 void hfree(void *ptr);
 
 #endif
-
-
-feat(heap): add heap interface
-
-- add hinit, halloc, hfree function definitions
-  - hinit: initialize heap
-  - halloc: allocate memory from heap
-  - hfree: free memory back to heap
